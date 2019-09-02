d093e8622cf158930fe93af190a38a7a3dbdabd2
Bruno Tavares de Moraes Russo <bruno.russo@evolutionit.com>
2019-09-02 17:26:14 -0300

    First commit all basic components added

---
+++ src/app/Poll/Poll.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Poll',
+  templateUrl: './Poll.component.html',
+  styleUrls: ['./Poll.component.css']
+})
+export class PollComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Login/Login.component';
+import { RegisterComponent } from './Register/Register.component';
+import { PollComponent } from './Poll/Poll.component';
+import { DashboardComponent } from './Dashboard/Dashboard.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      LoginComponent,
+      RegisterComponent,
+      PollComponent,
+      DashboardComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
